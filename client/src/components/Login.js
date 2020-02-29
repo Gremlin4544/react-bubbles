eccc2bea2ccf6061ef6e964d0a28f3fafcced413
Gremlin4544 <alve@mckethen.com>
2020-02-28 19:15:57 -0500

    created private route and axioswithauth files in utils directory, added content to login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,34 @@ import React from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
+  const [login, setLogin] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = event => {
+    setLogin({
+      ...login,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    axiosWithAuth()
+        .post('login', login)
+        .then(response => {
+            console.log("response", response)
+            localStorage.setItem('token', response.data.payload)
+            props.history.push("/colors"); 
+        // colors now has be made a protected route
+        // props.history.push re-directs you to another page
+        })
+        .catch(error => {
+            console.log(`login error: ${error}`);
+        });
+};
+
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
